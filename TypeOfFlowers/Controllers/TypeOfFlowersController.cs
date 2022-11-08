a5ccc3b909f81c24bf4d35cd4abc70c4f7824ddb
Aleksandra <aleksandrashatraj@gmail.com>
2022-11-08 13:27:39 +0100

    Add project files.

---
+++ TypeOfFlowers/Controllers/TypeOfFlowersController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TypeOfFlowers.Models;
+
+
+namespace TypeOfFlowers.Controllers
+{
+    public class TypeOfFlowersController : Controller
+    {
+        private static List<TypesViewModel> types= new List<TypesViewModel>();
+        public IActionResult Index()
+        {
+
+            return View(types);
+
+        }
+
+        public IActionResult Type()
+        {
+            var typeVm = new TypesViewModel();
+            return View(typeVm);
+        }
+
+
+
+        public IActionResult RegisterType(TypesViewModel typesViewModel)
+        {
+            types.Add(typesViewModel);
+            return RedirectToAction(nameof(Index));
+        }
+
+
+    }
+}
+
+
+
+
---
+++ TypeOfFlowers/Models/TypesViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TypeOfFlowers.Models
+{
+    public class TypesViewModel
+    {
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
